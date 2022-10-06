34a0820748dd7273890f3edb77ebb20afa43115a
montiho8 <montiho8@hawaii.edu>
2022-10-05 23:08:11 -1000

    Completed Hackwich Five Problem Set 1

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -45,8 +45,23 @@ class ViewController: UIViewController {
         else
         {
             print("button has been disabled")
-                  (buttonLabel!).isEnabled = false
+            (buttonLabel!).isEnabled = false
         }
+        
+        //Problem Set 1
+        //Line 39: creates the "if statement block" to currentIndex < favoriteFoodsArray.count
+        
+        //Line 41: if the statement on Line 39 is true then it will display the bottomLabel text to currentIndex/my first favorite food "Sushi" upon opening the app
+        
+        //Line 42: when the button is pressed, the button's text changes to "Next"
+        
+        //Line 43: increases the currentIndex by 1 to display my other favorite foods in the array
+        
+        //Line 45: creates the else statement block (extra comment)
+        
+        //Line 47: prints "button has been disabled" if the "if statement" is false
+        
+        //Line 48: disables the button when the user gets to the end of the array, user can no longer press the button
         
     }
     
